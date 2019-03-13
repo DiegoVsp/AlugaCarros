79949d89acf02c2d5d9cba21b3fceae4c0117cfb
Diego <diego-vespa2020@outlook.com.br>
2019-03-12 21:21:24 -0300

    ret

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Carro } from '../modelos/carro';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  
+  public carros: Carro[];
 
+  constructor(public http:HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.http.get<Carro[]>('http://localhost:8080/api/carro/listaTodos')
+    .subscribe(
+      (carros)=>{
+        this.carros = carros;
+      }
+    )
+  }
 }
