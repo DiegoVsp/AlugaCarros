6f96f065b7b2376811917601296b4259c70b5cad
Diego <diego-vespa2020@outlook.com.br>
2019-04-11 12:32:51 -0300

    ion_toggle_botao

--- src/app/escolha/escolha.page.ts
+++ src/app/escolha/escolha.page.ts
@@ -11,12 +11,16 @@ import { Carro } from '../modelos/Carro';
 })
 export class EscolhaPage implements OnInit {
   
-  public toggle:boolean = true;
+ 
 
 private carro: Carro
 
   constructor(private navCtrl: NavController,
     private activatedRoute: ActivatedRoute) { }
+
+    acessorios = [{acessorios: 'Freio Abs', preco:800},
+                  {acessorios: 'Ar-Condicionado', preco: 1000},
+                  {acessorios: 'MP3 player', preco: 500}]
 
   ngOnInit() {
 this.activatedRoute.queryParams
